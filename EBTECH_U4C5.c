46c0fa1a04150f58a30921d1575dc9101c81f5bd
Vitorhugofsousa <vitorhugofs2018@gmail.com>
2025-02-03 23:40:22 -0300

    adicionando o programa

--- EBTECH_U4C5.c
+++ EBTECH_U4C5.c
@@ -3,43 +3,56 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-//definindo os pinos dos leds
+//definindo os pinos dos leds e do botão
+#define BUTTON_A 5
 #define LED_PIN_RED 13
 #define LED_PIN_BLUE 12
 #define LED_PIN_GREEN 11
 
-//definindo a função de callback
-bool callback_timer(struct repeating_timer *t) {
+static bool programa_iniciado = false;
+static int estado_semaforo = 0;
+
+//definindo a função de alarme
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
     printf("Mudança de Cor\n");
     //desligando todos os leds
     gpio_put(LED_PIN_RED, 0);
     gpio_put(LED_PIN_BLUE, 0);
     gpio_put(LED_PIN_GREEN, 0);
 
-//definindo o estado do semáforo
-static int estado_semaforo = 0;
-
-    //verificando o estado do semáforo
-    switch (estado_semaforo) {  // 0 - Vermelho, 1 - Azul, 2 - Verde
+  
+    switch (estado_semaforo) { 
         case 0:
-            gpio_put(LED_PIN_BLUE, 1);
-            estado_semaforo = 1;
-            break;
+        gpio_put(LED_PIN_RED, 1);
+        gpio_put(LED_PIN_BLUE, 1);
+        gpio_put(LED_PIN_GREEN, 1);
+        break;
         case 1:
-            gpio_put(LED_PIN_GREEN, 1);
-            estado_semaforo = 2;
-            break;
+        gpio_put(LED_PIN_RED, 1);
+        gpio_put(LED_PIN_BLUE, 1);
+        break;
         case 2:
-            gpio_put(LED_PIN_RED, 1);
-            estado_semaforo = 0;
-            break;
+        gpio_put(LED_PIN_RED, 1);
+        break;
+
     }
-    return 1;
+
+    estado_semaforo = (estado_semaforo + 1) % 3; 
+
+
+if (estado_semaforo == 0) { 
+        
+        printf("Programa encerrado. Aguardando pressionar o botão...\n");
+        programa_iniciado = false;
+        return 0; 
+    }
+    add_alarm_in_ms(3000, alarm_callback, NULL, false); 
+    return 0;
 }
 
 //função principal
 int main(){
-    //inicializando os pinos dos leds
+    //inicializando os pinos dos leds e o botão
     stdio_init_all();
     gpio_init(LED_PIN_GREEN);
     gpio_set_dir(LED_PIN_GREEN, GPIO_OUT);
@@ -47,15 +60,23 @@ int main(){
     gpio_set_dir(LED_PIN_BLUE, GPIO_OUT);
     gpio_init(LED_PIN_RED);
     gpio_set_dir(LED_PIN_RED, GPIO_OUT);
-
-    //criando um timer
-    struct repeating_timer timer;   
-    add_repeating_timer_ms(3000, callback_timer, NULL, &timer);
-    gpio_put(LED_PIN_RED, 1);   //inicializando o led vermelho no inicio do programa
-
+    gpio_init(BUTTON_A);
+    gpio_set_dir(BUTTON_A, GPIO_IN);
+    gpio_pull_up(BUTTON_A);
+   
+     printf("Aguardando pressionar o botão...\n");  //mensagem de espera
     //loop infinito
     while (1) {
-        sleep_ms(1000);
-        printf("Se passou um Segundo!(1)\n");   //imprimindo a cada segundo
+         if (!gpio_get(BUTTON_A) && !programa_iniciado) {
+            sleep_ms(100);
+            if(!gpio_get(BUTTON_A)){
+                printf("Botão Pressionado, Ligando as LEDs!\n");
+                programa_iniciado = true;
+                estado_semaforo = 0;
+                alarm_callback(0, NULL);
+            }
     }
+sleep_ms(10);
 }
+}
+
